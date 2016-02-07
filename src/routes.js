7da37954a8c5f3cd32e02544cc00465ee5c738cb
Caleb Thorsteinson <caleb@thorsteinson.me>
2016-02-07 02:11:04 +0000

    Used try catch style in router
    Fixes #1

--- src/routes.js
+++ src/routes.js
@@ -6,23 +6,20 @@ export const course = new Router()
 
 course.post('/', function *(next) {
   // The body needs to have an object describing a course
-  yield db.addCourse(this.request.body)
-    .then(([id]) => {
-      this.status = 201
-      this.body = id.toString()
-    })
-    .catch(err => {
-      this.throw(400, err)
-    })
+  try {
+    const [id] = yield db.addCourse(this.request.body)
+    this.status = 201
+    this.body = id.toString()
+  } catch (err) {
+    this.throw(400, err)
+  }
 })
 
 course.get('/:id', function *(next) {
   // Gets a course with the selected id
-  yield db.getCourse(this.params.id)
-    .then(course => {
-      this.body = course
-    })
-    .catch(err => {
-      this.throw(400, err)
-    })
+  try {
+    this.body = yield db.getCourse(this.params.id)
+  } catch (err) {
+    this.throw(400, err)
+  }
 })
